459d2fbb3373497857a09c8742a933099d3633ea
Vislavath Gopal <vislavathgopal644@gmail.com>
2026-09-21 18:49:34 +0530

    Use same-domain Vercel API

--- src/api/studentAPI.js
+++ src/api/studentAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = (process.env.REACT_APP_API_URL || 'https://student-management-system-backend-e521.onrender.com').replace(/\/$/, '');
+const API_URL = (process.env.REACT_APP_API_URL || '/api').replace(/\/$/, '');
 const BASE_URL = `${API_URL}/students`;
 
 export const getAllStudents = async () => {
